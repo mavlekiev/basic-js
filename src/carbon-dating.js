16ae4eb648dd9320aaeca6b43d2cb307f564b4f6
mavlekiev <mavlekiev@gmail.com>
2024-12-29 22:17:46 +0700

    feat: add solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,9 +18,9 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
 function dateSample(sampleActivity) {
-  if (!Number.isInteger(+sampleActivity) || +sampleActivity <= 0) return false;
-  return Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / (0.693 / 5730));
-  // remove line with error and write your code here
+  if (typeof sampleActivity !== 'string' || +sampleActivity <= 0 || typeof +sampleActivity !== 'number' || isNaN(+sampleActivity) || +sampleActivity > 15) return false;
+  sampleActivity = +sampleActivity;
+  return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / 5730));
 }
 
 module.exports = {
